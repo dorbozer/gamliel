857b6e2419275f524a22ff446bb69ea660cb4835
Dor <dor.biztrizer@gmail.com>
2020-10-02 14:36:38 +0300

    V.6

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
 });
 
 
-/*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
+/*========== CLOSE MOBILE MENU ON CLICK ==========*/
 $(document).ready(function () {
   $(document).click(function (event) {
       var clickover = $(event.target);
@@ -29,6 +29,35 @@ $(document).ready(function () {
       if (_opened === true && !clickover.hasClass("navbar-toggler")) {
           $(".navbar-toggler").click();
       }
+  });
+});
+
+
+/*========== SMOOTH SCROLLING TO LINKS ==========*/
+
+$(document).ready(function(){ 
+  $("a").on('click', function(event) {//click on any link;anchor tag;
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") { //for e.g. website.com#home - #home
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+      //console.log('hash:',hash)
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({ //animate whole html and body elements
+        scrollTop: $(hash).offset().top //scroll to the element with that hash
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash; //website.com - website.com#home
+        //Optional remove "window.location.hash = hash;" to prevent transparent navbar on load
+      });
+    } // End if
   });
 });
 
@@ -71,3 +100,4 @@ $(function () { // a self calling function
   onScrollInit($('.os-animation')); //function call with only items
   onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
 });
+
